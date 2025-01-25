8520ff371d7182298beec94675430cee795536e2
SaurabhKumarJ <sa10skj05@gmail.com>
2025-01-26 01:55:53 +0530

    Metadata update

--- src/Deployment/app.py
+++ src/Deployment/app.py
@@ -20,7 +20,7 @@ faviconPath = "../Deployment/images/favicon.png"
 #Changing metadata
 st.set_page_config(
         page_title='FixMyStreet',
-        page_icon=faviconPath,
+        page_icon='🛣️',
         layout="wide",   
         initial_sidebar_state="expanded",              
         )
